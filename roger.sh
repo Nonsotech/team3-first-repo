25bc8344ced4dba90270c9967656d5731554bfa2
rachiamb <rachiamb_spivey@yahoo.com>
2022-08-21 07:35:20 +0200

    new scriptfile created

---
+++ roger.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#author enginer roger
+#this script is to perform a system analysis
+echo "welcome to Etech Consulting Devops Masterclass"
+sudo apt update -y
+df -h
+lscpu
+free -g
+uptime
